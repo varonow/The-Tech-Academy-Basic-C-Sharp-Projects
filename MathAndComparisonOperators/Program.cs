d6d5610e8857aaeab78bc92d61452694fe49c2e0
Victoria Aronow <varonow@mettel.net>
2020-02-02 16:06:40 -0500

    Add data conversions Add comparison practice

---
+++ MathAndComparisonOperators/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Takes an input from the user, multiplies it by 50, and prints the result to the console.
+            // (Note: make sure your code can take inputs larger than 10,000,000).
+
+            Console.WriteLine("Please enter a whole number to multiply it by 50:");
+            int toMultiply = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"{toMultiply} * 50 = {toMultiply * 50}\n");
+
+            // Takes an input from the user, adds 25 to it, and prints the result to the console.
+
+            Console.WriteLine("Please enter a whole number to add 25 to it:");
+            int toAdd = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"{toAdd} + 25 = {toAdd + 25}\n");
+
+            // Takes an input from the user, divides it by 12.5, and prints the result to the console.
+
+            Console.WriteLine("Please enter a number to divide it by 12.5:");
+            double toDivide = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{toDivide} / 12.5 = {toDivide / 12.5}\n");
+
+            // Takes an input from the user, checks if it is greater than 50, and prints the true/false 
+            // result to the console.
+
+            Console.WriteLine("Enter a whole number to see if it is greater than 50");
+            int toCompareToFifty = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"{toCompareToFifty} > 50 ? {toCompareToFifty > 50}\n");
+
+            // Takes an input from the user, divides it by 7, and prints the remainder to the console 
+            // (tip: think % operator).
+
+            Console.WriteLine("Enter a number to see the remainder after dividing by 7");
+            double divideBySeven = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{divideBySeven} / 7 has a remainder of {divideBySeven % 7}\n");
+
+
+            Console.Read();
+        }
+    }
+}
--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -18,22 +18,22 @@
             Console.WriteLine("What course are you on?");
             string userCourse = Console.ReadLine();
 
-            Console.WriteLine("What page number?");
-            string userPageNum = Console.ReadLine();
+            Console.WriteLine("\nWhat page number?");
+            int userPageNum = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
-            string userNeedHelp = Console.ReadLine();
+            Console.WriteLine("\nDo you need help with anything? Please answer \"true\" or \"false\".");
+            bool userNeedHelp = bool.Parse(Console.ReadLine());
 
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.WriteLine("\nWere there any positive experiences you’d like to share? Please give specifics.");
             string userPositiveInput = Console.ReadLine();
 
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.WriteLine("\nIs there any other feedback you’d like to provide? Please be specific.");
             string userMiscInput = Console.ReadLine();
 
-            Console.WriteLine("How many hours did you study today?");
-            string userHoursStudied = Console.ReadLine();
+            Console.WriteLine("\nHow many hours did you study today?");
+            int userHoursStudied = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Thank you for your answers.\nAn Instructor will respond to this shortly.\nHave a great day!");
+            Console.WriteLine("\nThank you for your answers.\nAn Instructor will respond to this shortly.\nHave a great day!");
 
             Console.ReadLine();
         }
